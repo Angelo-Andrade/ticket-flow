f6b3802d94681698087d189841552d08fd19bfdb
Ângelo <121000234+Angelo-Andrade@users.noreply.github.com>
2024-11-13 20:21:37 -0300

    resolvendo conflitos
    conflitos apresentados entre a implementação de pesquisa e a configuração do notfound

--- app/controllers/verificar_chamados.js
+++ app/controllers/verificar_chamados.js
@@ -1,5 +1,6 @@
-const dbConnection = require('../../config/dbConnection');
-const { verificar_chamados_todos, verificar_chamados_filtrados } = require('../models/verificar_chamados');
+const  dbConnection = require("../../config/dbConnection");
+const { closeConnection } = require("../../config/dbConnection") 
+const { verificar_chamados_todos, verificar_chamados_filtrados } = require("../models/verificar_chamados");
 
 module.exports.verificar_chamados_todos = (app, req, res) => {
   console.log('[Controller verificar_chamados]');
@@ -13,32 +14,21 @@ module.exports.verificar_chamados_todos = (app, req, res) => {
   }
     else {
       console.log(result);
-      res.render('verificar_chamados.ejs', { tickets: result });
+      res.render('verificar_chamados.ejs', { tickets: result, filter: undefined });
 
     }
   });
+  closeConnection(dbConn);
 };
 
 module.exports.verificar_chamados_filtrados = (app, req, res) => {
   console.log('[Controller verificar_chamados]');
   dbConn = dbConnection();
-  const filter = req.query.status;
+  const filter = req.query.filtro;
+  const filter_value = req.query.valor;
   verificar_chamados_filtrados(dbConn, filter, (error, result) => {
     console.log(result);
     res.render('verificar_filtrados.ejs', { tickets: result, filter: filter });
   });
+  closeConnection(dbConn);
 };
-
-// module.exports.addComment = (app, req, res) => {
-//   console.log('[Controller Home] Save Comment');
-//   const idObra = req.query.idobra;
-//   const comment = req.body.comment;
-//   console.log('idObra', idObra);
-//   console.log('Comment', comment);
-//   dbConn = dbConnection();
-//   addComment(dbConn, idObra, comment, (error, result) => {
-//     console.log(error);
-//     console.log(result);
-//     res.redirect('/');
-//   });
-// };
