23b688754e1b91f2a6dbd9d722ea26a255d5890e
Angelo <henriqueangelo0611@gmail.com>
2024-12-03 01:19:23 -0300

    Enconbrindo dados do banco de dados

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,16 +1,20 @@
 const mysql = require("mysql2");
+require('dotenv').config({ path: './config/infoconn.env' });
 
-const host = "sql10.freesqldatabase.com";
-const database = "sql10744601";
-const user = "sql10744601";
-const password = "BZAtPjn7Gi";
+const host = process.env.DB_HOST;
+const port = process.env.DB_PORT;
+const database = process.env.DB_DATABASE;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+
 
 module.exports = () => {
   return (dbConnection = mysql.createConnection({
     host: host,
+    port: port,
     user: user,
     password: password,
-    database: database,
+    database: database
   }));
   console.log('[dbConnection] conexão aberta');
 };
