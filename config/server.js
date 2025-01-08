2c7e9b4e420433b633077237853ab22256201cb0
Angelo-Andrade <henriqueangelo0611@gmail.com>
2025-01-07 22:45:42 -0300

    implementando banco de sessões

--- config/server.js
+++ config/server.js
@@ -6,17 +6,31 @@ dovenv.config();
 
 let expressSession = require('express-session');
 
+const RedisStore = require('connect-redis').RedisStore;
+
+const { createClient } = require('./cacheConnection');
+const client = createClient();
+client.connect().catch(console.error);
+
+
 let port = 3000;
 
 app.set("views", "./app/views");
 app.set("view engine", "ejs");
 app.use(express.static("./public"));
 app.use(express.urlencoded({ extended: true }));
+
 app.use(expressSession({
-	secret: process.env.SECRET,
-	name: 'uniqueSessionID',
-	resave: false,
-	saveUninitialized: false
+    store: new RedisStore({ client }),
+    secret: process.env.SECRET,
+    name: 'uniqueSessionID',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false,
+        httpOnly: true,
+		maxAge: 1000 * 60 * 5
+    },
 }));
 
 app.listen(port, function () {
